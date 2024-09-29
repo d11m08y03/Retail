52cde52d266597f0d77dfd8d95c464e9c32c7863
ihsaan <mohammedihsaan15@gmail.com>
2024-09-29 15:37:14 +0400

    add home.c to compilation

--- client/build.sh
+++ client/build.sh
@@ -1,3 +1,3 @@
-gcc main.c components/navbar.c components/card.c pages/products.c -Lextern -lraygui -lraylib -Iextern -lm -o bin
+gcc main.c components/navbar.c components/card.c pages/products.c pages/about.c -Lextern -lraygui -lraylib -Iextern -lm -o bin
 
 export LD_LIBRARY_PATH=./extern/:$LD_LIBRARY_PATH && ./bin
