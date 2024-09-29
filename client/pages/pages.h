d9c68da3407af705435d21e0c0e62b7c33816b19
ihsaan <mohammedihsaan15@gmail.com>
2024-09-29 15:36:26 +0400

    restructure pages enum

--- client/pages/pages.h
+++ client/pages/pages.h
@@ -1,6 +1,6 @@
 #ifndef PAGES_H
 #define PAGES_H
 
-typedef enum { PAGE_PRODUCTS, PAGE_ABOUT, PAGE_LOGIN, PAGE_SIGNUP } PageType;
+typedef enum { PAGES_HOME, PAGE_PRODUCTS, PAGE_LOGIN, PAGE_SIGNUP } PageType;
 
 #endif // !PAGES_H
