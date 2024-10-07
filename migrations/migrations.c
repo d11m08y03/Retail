f1cf3d153a6a6a7f7ff8f03403648ffc812b724b
d11m08y03 <zakariyyakurmally@gmail.com>
2024-10-07 13:10:08 +0400

    update to handle new migrations

--- migrations/migrations.c
+++ migrations/migrations.c
@@ -53,38 +53,28 @@ int main(void) {
     return 1;
   }
 
-  const char *sql_file_01 = "./001_create_users.sql";
-  rc = run_migration(db, sql_file_01);
+  // List of migration files to run
+  const char *migration_files[] = {
+      "./migrations/001_create_users.sql",
+      "./migrations/002_create_products.sql",
+      "./migrations/003_create_orders_table.sql",
+      "./migrations/004_create_order_items_table.sql",
+      "./migrations/005_populate_users.sql",
+      "./migrations/006_populate_products.sql",
+      "./migrations/007_populate_orders.sql",
+      "./migrations/008_populate_order_items.sql"};
 
-  if (rc != SQLITE_OK) {
-    fprintf(stderr, "Migration 01 failed.\n");
-    sqlite3_close(db);
-    return 1;
+  // Run each migration file
+  for (size_t i = 0; i < sizeof(migration_files) / sizeof(migration_files[0]);
+       i++) {
+    rc = run_migration(db, migration_files[i]);
+    if (rc != SQLITE_OK) {
+      fprintf(stderr, "Migration %02zu failed.\n", i + 1);
+      sqlite3_close(db);
+      return 1;
+    }
+    printf("Migration %02zu ran successfully.\n", i + 1);
   }
-
-  printf("Migration ran 01 successfully.\n");
-
-  const char *sql_file_02 = "./002_create_products.sql";
-  rc = run_migration(db, sql_file_02);
-
-  if (rc != SQLITE_OK) {
-    fprintf(stderr, "Migration 02 failed\n");
-    sqlite3_close(db);
-    return 1;
-  }
-
-  printf("Migration ran 02 successfully\n");
-
-  const char *sql_file_03 = "./001_create_users.sql";
-  rc = run_migration(db, sql_file_03);
-
-  if (rc != SQLITE_OK) {
-    fprintf(stderr, "Migration 03 failed.\n");
-    sqlite3_close(db);
-    return 1;
-  }
-
-  printf("Migration ran 03 successfully.\n");
 
   sqlite3_close(db);
   return 0;
