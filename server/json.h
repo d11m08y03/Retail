f2794b1eeaa50bc9e0f6974bfe4097c42f7d52e0
Boomin999 <mohammedihsaan15@gmail.com>
2024-10-10 21:00:36 +0400

    add logs and value extraction

--- server/json.h
+++ server/json.h
@@ -20,5 +20,7 @@ char *json_stringify(const json_object_t *obj);
 void json_free_object(json_object_t *obj);
 char *json_from_body(const char *buffer);
 json_object_t *json_from_string(const char *json_string);
+void json_print_object(const json_object_t *obj);
+char *json_get_value_by_key(const json_object_t *obj, const char *key);
 
 #endif // !JSON_H
